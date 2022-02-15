af110fcb7457444b07b1d51c049490f07a029b83
Rofer11607 <rafac@monsterrg.com>
2022-02-15 11:07:06 -0500

    fixed permissions issue, I was downgraded from admin to user. new api key needed to be made.

--- functions/src/Entrypoints/CreateTimesheet/CreateTimesheet.LX.ts
+++ functions/src/Entrypoints/CreateTimesheet/CreateTimesheet.LX.ts
@@ -1,18 +1,18 @@
-import {TimeSheet_} from './main.spec'
-import axios from 'axios'
+import axios from "axios";
+import { TimeSheet_ } from "./main.spec";
 
 /* <-- Import End --> */
 
 const CONFIG = {
-     url : "https://rest.tsheets.com/api/v1/timesheets",
-     headers : {
-        Authorization: "Bearer S.15__35c00571a09a30d0dc06b11d4f42d76d3ec33f16",
-    }
-}
+    url: "https://rest.tsheets.com/api/v1/timesheets",
+    headers: {
+        Authorization: "Bearer S.15__5463f8eadbf6b8749cb79719b967f54da30d402a",
+    },
+};
 
 /* <-- Variable Definition End --> */
 
 export function createTimesheetsInQuickbooks(input: TimeSheet_) {
-    const {headers,url} = CONFIG
-    return axios.post(url, input, {headers});
+    const { headers, url } = CONFIG;
+    return axios.post(url, input, { headers });
 }
